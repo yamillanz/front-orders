0cfa563be57ef1178c6892b05db8a8797f49602b
Yamil Lanz <yamil.lanz@naranja.com>
2022-07-15 11:04:06 -0400

    orders components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+import { OrderListComponent } from './orders/components/order-list/order-list.component';
+import { OrderDetailsComponent } from './orders/components/order-details/order-details.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, OrderListComponent, OrderDetailsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
