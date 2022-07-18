dd94bb10518ee534d907d799ef870011539ef6a1
Yamil Lanz <yamil.lanz@naranja.com>
2022-07-18 07:06:17 -0400

    formating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,19 @@ import { HeaderComponent } from './shared/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { OrderListComponent } from './orders/components/order-list/order-list.component';
 import { OrderDetailsComponent } from './orders/components/order-details/order-details.component';
+import { ProductsListComponent } from './products/components/products-list/products-list.component';
+import { ProductDetailsComponent } from './products/components/product-details/product-details.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, OrderListComponent, OrderDetailsComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    OrderListComponent,
+    OrderDetailsComponent,
+    ProductsListComponent,
+    ProductDetailsComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
